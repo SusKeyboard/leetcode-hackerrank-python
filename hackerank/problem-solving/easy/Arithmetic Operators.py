638d59c6a77e3d5b62a049fc01f72ead7f5e656b
SusKeyboard <105714378+SusKeyboard@users.noreply.github.com>
2026-01-24 19:48:40 +0530

    Add Arithmetic Operators solution
    Implement basic arithmetic operations for two integers.

---
+++ hackerank/problem-solving/easy/Arithmetic Operators.py
@@ -0,0 +1,13 @@
+"""
+Platform: HackerRank
+Problem: Arithmatic Operations
+Difficulty: Easy
+Concepts: Operators
+"""
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
