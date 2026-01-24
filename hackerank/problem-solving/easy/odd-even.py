32a5c458009884a46f0b79506739c8ae2a3ba893
SusKeyboard <105714378+SusKeyboard@users.noreply.github.com>
2026-01-24 19:41:25 +0530

    Add odd-even problem solution
    Implement odd-even decision logic based on input value.

---
+++ hackerank/problem-solving/easy/odd-even.py
@@ -0,0 +1,19 @@
+"""
+Platform: HackerRank
+Problem: Odd even tasks
+Difficulty: Easy
+Concepts: if else statements
+"""
+
+n = int(input())
+
+if n%2 == 1:
+    print("Weird")
+
+elif n%2 == 0:
+    if 2<= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
